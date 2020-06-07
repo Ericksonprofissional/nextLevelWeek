2b94de5e15121cdb8ec80b24e83b89a6500fcc80
ericksonprofissional <ericksonprofissional@gmail.com>
2020-06-07 11:49:32 -0400

    Pagina inicial e configurando a pasta public

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ const server = express();
 
 server.listen(3000);
 
+// congigurar pasta publica
+
+server.use(express.static("public"))
+
 //pagina inicial
 server.get("/", (req, res) => {
-    res.send("Cheguei aqui")
+    res.sendFile(__dirname+ "/views/index.html")
 })
