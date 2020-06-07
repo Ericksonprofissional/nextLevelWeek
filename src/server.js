61172f26e116115f6e46cfeec56d3faa7e238c9a
ericksonprofissional <ericksonprofissional@gmail.com>
2020-06-07 12:15:12 -0400

    Construindo as rotas das paginas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 
+// iniciar servidor
 server.listen(3000);
 
 // congigurar pasta publica
@@ -10,4 +11,13 @@ server.use(express.static("public"))
 //pagina inicial
 server.get("/", (req, res) => {
     res.sendFile(__dirname+ "/views/index.html")
-})
+});
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html");
+});
+
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/searchResults.html");
+});
+
