1e632a314043d64bffe7bef87d09e229c5f633e4
ericksonprofissional <ericksonprofissional@gmail.com>
2020-06-07 11:44:53 -0400

    Pagina inicial

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 const express = require("express");
-const server = express()
+const server = express();
 
 server.listen(3000);
+
+//pagina inicial
+server.get("/", (req, res) => {
+    res.send("Cheguei aqui")
+})
